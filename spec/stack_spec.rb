d3df1e171d5e9b5dd1e5d03f61071e16be7b17ec
h7kayama <0hideki0@gmail.com>
2018-08-25 15:36:25 +0900

    No.3 done

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Stack do
   let(:stack) { Stack.new }
   describe "#push" do
-    subject { stack.push }
+    subject { stack.push 1 }
     it { expect(subject).to eq stack }
   end
 end
