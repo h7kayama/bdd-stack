218ad0d7efa20f80f7350c6fc53003dbb3f464d3
h7kayama <0hideki0@gmail.com>
2018-08-25 15:44:55 +0900

    popの実装

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -7,6 +7,29 @@ RSpec.describe Stack do
     it { expect(subject).to eq stack }
   end
 
+  describe "#pop" do
+    subject { stack.pop }
+    context "空のstack" do
+      it { expect(subject).to be_nil }
+      it { expect{subject}.not_to change{stack.length} }
+    end
+
+    context "1をpush済みのstack" do
+      before { stack.push 1 }
+      it { expect(subject).to eq 1 }
+      it { expect{subject}.to change{stack.length}.from(1).to(0) }
+    end
+
+    context "1と5をpush済みのstack" do
+      before do
+        stack.push 1
+        stack.push 5
+      end
+      it { expect(subject).to eq 5 }
+      it { expect{subject}.to change{stack.length}.from(2).to(1) }
+    end
+  end
+
   describe "#length" do
     subject { stack.length }
     context "1をpush済みのstack" do
