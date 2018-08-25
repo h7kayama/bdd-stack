9c1c0b6d6cd7767fdfeaf2689eb7b8b8a439caa6
h7kayama <0hideki0@gmail.com>
2018-08-25 15:40:25 +0900

    No.6 done

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe Stack do
         it { expect(subject).to eq 1 }
       end
     end
+
+    context "1と5をpush済みのstack" do
+      before do
+        stack.push 1
+        stack.push 5
+      end
+      subject { stack.length }
+      describe "lengthが2" do
+        it { expect(subject).to eq 2 }
+      end
+    end
   end
 end
